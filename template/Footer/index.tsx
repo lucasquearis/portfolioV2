c446e0baa0a454ad64fb2612b7b45f0c46d52a41
lucasquearis <lucasquearis@hotmail.com>
2022-10-30 19:47:34 -0300

    feat: adiciona animacoes no portfolio todo

--- template/Footer/index.tsx
+++ template/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="flex h-auto min-h-[70px] flex-col justify-center gap-4 bg-primary-background-primary fill-primary-text-navy-navy p-4 text-center text-sm text-primary-text-navy-navy">
       <Bounce bottom>
-        <div className="flex w-full justify-center gap-5 after:bg-primary-text-navy-navy md:fixed md:right-auto md:left-5 md:bottom-0 md:z-10 md:w-10 md:flex-col md:after:my-0 md:after:mx-auto md:after:h-24 md:after:w-[1px]">
+        <div className="hidden w-full justify-center gap-5 after:bg-primary-text-navy-navy md:fixed md:right-auto md:left-5 md:bottom-0 md:z-10 md:flex md:w-10 md:flex-col md:after:my-0 md:after:mx-auto md:after:h-24 md:after:w-[1px]">
           <a
             href="https://github.com/lucasquearis"
             target="_blank"
@@ -32,7 +32,29 @@ export default function Footer() {
           </a>
         </div>
       </Bounce>
-
+      <div className="flex w-full justify-center gap-5 after:bg-primary-text-navy-navy md:fixed md:right-auto md:left-5 md:bottom-0 md:z-10 md:hidden md:w-10 md:flex-col md:after:my-0 md:after:mx-auto md:after:h-24 md:after:w-[1px]">
+        <a
+          href="https://github.com/lucasquearis"
+          target="_blank"
+          className="fill-primary-text-navy-navy transition-all duration-300 hover:-translate-y-1 hover:fill-primary-text-primary hover:text-primary-text-primary md:mx-auto"
+        >
+          <GithubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lucasquearis/"
+          target="_blank"
+          className="fill-primary-text-navy-navy transition-all duration-300 hover:-translate-y-1 hover:fill-primary-text-primary hover:text-primary-text-primary md:mx-auto"
+        >
+          <Linkedinicon />
+        </a>
+        <a
+          href="https://www.instagram.com/lucasquearis/"
+          target="_blank"
+          className="fill-primary-text-navy-navy transition-all duration-300 hover:-translate-y-1 hover:fill-primary-text-primary hover:text-primary-text-primary md:mx-auto"
+        >
+          <InstagramIcon />
+        </a>
+      </div>
       <Bounce bottom>
         <div className="email fixed right-5 left-auto bottom-0 z-10 hidden w-10 items-center justify-center gap-5 text-center font-mono font-semibold tracking-widest after:my-0 after:mx-auto after:h-24 after:w-[1px] after:bg-primary-text-navy-navy md:flex">
           <a
--- template/Welcome/index.tsx
+++ template/Welcome/index.tsx
@@ -1,22 +1,26 @@
+import { Fade } from "react-awesome-reveal";
+
 export default function Welcome() {
   return (
     <section className="mx-auto flex min-h-screen max-w-5xl flex-col items-start justify-center gap-3 p-0">
-      <p className="font-mono text-lg text-primary-text-primary">
-        Hi, my name is
-      </p>
-      <h2 className="text-4xl font-bold text-primary-text-navy-light md:text-6xl lg:text-7xl">
-        Lucas A. Santos.
-      </h2>
-      <h3 className="text-4xl font-bold text-primary-text-navy-navy md:text-6xl lg:text-7xl">
-        I'm Front-end Developer
-      </h3>
-      <p className="max-w-xl text-primary-text-navy-navy md:text-xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit impedit
-        veniam ab consectetur quos molestias ratione assumenda officia, esse
-      </p>
-      <button className="w-full rounded-md border-2 border-primary-text-primary p-4 font-mono text-primary-text-primary transition-colors hover:bg-primary-text-opacity md:w-auto">
-        Download my CV!
-      </button>
+      <Fade cascade damping={0.2} delay={400} triggerOnce>
+        <p className="font-mono text-lg text-primary-text-primary">
+          Hi, my name is
+        </p>
+        <h2 className="text-4xl font-bold text-primary-text-navy-light md:text-6xl lg:text-7xl">
+          Lucas A. Santos.
+        </h2>
+        <h3 className="text-4xl font-bold text-primary-text-navy-navy md:text-6xl lg:text-7xl">
+          I'm Front-end Developer
+        </h3>
+        <p className="max-w-xl text-primary-text-navy-navy md:text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit impedit
+          veniam ab consectetur quos molestias ratione assumenda officia, esse
+        </p>
+        <button className="w-full rounded-md border-2 border-primary-text-primary p-4 font-mono text-primary-text-primary transition-colors hover:bg-primary-text-opacity md:w-auto">
+          Download my CV!
+        </button>
+      </Fade>
     </section>
   );
 }
